d3391629db3f1886a64d114fbc705c5553aaecc3
Lucas Stopinski da Silva <160746304+LucasStop@users.noreply.github.com>
2025-03-29 16:12:25 -0300

    feat: remover criação de diretórios desnecessária para arquivos de credenciais e dados de usuário

--- core/credentials.py
+++ core/credentials.py
@@ -1,27 +1,18 @@
 # core/credentials.py
 import os
 import json
-from constants import CREDENTIALS_FILE
+
+CREDENTIALS_FILE = "data/credentials.json"
 
 def initialize_credentials():
-    directory = os.path.dirname(CREDENTIALS_FILE)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-        
     if not os.path.exists(CREDENTIALS_FILE):
         with open(CREDENTIALS_FILE, 'w') as file:
             json.dump({}, file)
 
 def load_credentials():
-    if not os.path.exists(CREDENTIALS_FILE):
-        initialize_credentials()
     with open(CREDENTIALS_FILE, 'r') as file:
         return json.load(file)
 
 def save_credentials(credentials):
-    directory = os.path.dirname(CREDENTIALS_FILE)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-        
     with open(CREDENTIALS_FILE, 'w') as file:
         json.dump(credentials, file, indent=4)
--- core/user_data.py
+++ core/user_data.py
@@ -3,18 +3,18 @@ import os
 import json
 import time
 from datetime import datetime
-from constants import USER_DATA_FILE
+
+USER_DATA_FILE = "data/user_data.json"
 
 def load_user_data():
     try:
         if not os.path.exists(USER_DATA_FILE):
-            os.makedirs(os.path.dirname(USER_DATA_FILE), exist_ok=True)  # Garante que o diretório existe
             with open(USER_DATA_FILE, 'w') as file:
                 json.dump({}, file)
         with open(USER_DATA_FILE, 'r') as file:
             return json.load(file)
     except json.JSONDecodeError:
-        print(f"Erro: Arquivo de dados corrompido. Criando backup e iniciando novo arquivo.")
+        print("Erro: Arquivo de dados corrompido. Criando backup e iniciando novo arquivo.")
         if os.path.exists(USER_DATA_FILE):
             backup_name = USER_DATA_FILE + f".bak.{int(time.time())}"
             os.rename(USER_DATA_FILE, backup_name)
@@ -26,6 +26,5 @@ def load_user_data():
         return {}
 
 def save_user_data(user_data):
-    os.makedirs(os.path.dirname(USER_DATA_FILE), exist_ok=True)  # Garante que o diretório existe
     with open(USER_DATA_FILE, 'w') as file:
         json.dump(user_data, file, indent=4)
